ed99039a15bafcba9b2b66141890e23f836c6c2f
shon5544 <sbs8239@gmail.com>
2023-09-13 18:12:41 +0900

    Initial Commit

---
+++ src/main/kotlin/com/yourssu/assignmentblog/AssignmentBlogApplication.kt
@@ -0,0 +1,11 @@
+package com.yourssu.assignmentblog
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AssignmentBlogApplication
+
+fun main(args: Array<String>) {
+	runApplication<AssignmentBlogApplication>(*args)
+}
