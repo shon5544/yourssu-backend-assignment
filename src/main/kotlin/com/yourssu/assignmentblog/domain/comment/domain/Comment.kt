5bdf00d0fc021e8721fc668810f0c82a0840e7b8
shon5544 <sbs8239@gmail.com>
2023-09-14 05:37:08 +0900

    refactor: BaseCreateAndUpdateTimeEntity 상속

--- src/main/kotlin/com/yourssu/assignmentblog/domain/comment/domain/Comment.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/comment/domain/Comment.kt
@@ -1,7 +1,7 @@
 package com.yourssu.assignmentblog.domain.comment.domain
 
+import com.yourssu.assignmentblog.domain.common.BaseCreateAndUpdateTimeEntity
 import com.yourssu.assignmentblog.domain.user.domain.User
-import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
@@ -11,12 +11,9 @@ class Comment(
     @Column(nullable = false, name = "comment_id")
     val id: Long? = null,
 
-    val createdAt: LocalDateTime? = null,
-
-    var updatedAt: LocalDateTime? = null,
     var content: String? = null,
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     val user: User
-)
+) : BaseCreateAndUpdateTimeEntity()
--- src/main/kotlin/com/yourssu/assignmentblog/domain/user/domain/User.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/user/domain/User.kt
@@ -1,8 +1,8 @@
 package com.yourssu.assignmentblog.domain.user.domain
 
 import com.yourssu.assignmentblog.domain.comment.domain.Comment
+import com.yourssu.assignmentblog.domain.common.BaseCreateAndUpdateTimeEntity
 import com.yourssu.assignmentblog.global.common.enums.Role
-import java.time.LocalDateTime
 import javax.persistence.*
 
 
@@ -13,12 +13,6 @@ class User(
     @Column(nullable = false, name = "user_id")
     val id: Long? = null,
 
-    @Column(nullable = false, name = "created_at")
-    val createdAt: LocalDateTime? = null,
-
-    @Column(nullable = false, name = "updated_at")
-    var updatedAt: LocalDateTime? = null,
-
     var email: String = "",
     var password: String = "",
     var username: String = "",
@@ -28,7 +22,7 @@ class User(
 
     @OneToMany(mappedBy = "user", cascade = [CascadeType.REMOVE])
     val commentList: List<Comment> = ArrayList()
-) {
+) : BaseCreateAndUpdateTimeEntity() {
     fun updateRefreshToken(refreshToken: String) {
         this.refreshToken = refreshToken
     }
