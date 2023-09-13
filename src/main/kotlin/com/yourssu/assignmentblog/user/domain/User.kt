828306e0a65f1882b6c5dd4ff8ddc1e1d67c07d2
shon5544 <sbs8239@gmail.com>
2023-09-13 20:09:41 +0900

    feat: User 엔티티 필드 추가

--- src/main/kotlin/com/yourssu/assignmentblog/user/domain/User.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/user/domain/User.kt
@@ -1,5 +1,25 @@
 package com.yourssu.assignmentblog.user.domain
 
-class User {
+import java.time.LocalDateTime
+import javax.persistence.*
 
-}
+
+@Entity
+class User(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    val id: Long?,
+
+    @Column(nullable = false, name = "created_at")
+    val createdAt: LocalDateTime,
+
+    @Column(nullable = false, name = "updated_at")
+    val updatedAt: LocalDateTime,
+
+    val email: String,
+    val password: String,
+    val username: String,
+
+    // 연관관계 매핑은 나중에
+)
