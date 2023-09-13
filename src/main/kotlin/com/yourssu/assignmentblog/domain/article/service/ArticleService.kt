387bc9e44b6f14f1af937498e10a9aaf8b4c31dc
shon5544 <sbs8239@gmail.com>
2023-09-14 08:32:36 +0900

    feat: ArticleService edit 메서드 추가

--- src/main/kotlin/com/yourssu/assignmentblog/domain/article/service/ArticleService.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/article/service/ArticleService.kt
@@ -9,6 +9,7 @@ import com.yourssu.assignmentblog.global.common.exception.CustomException
 import org.springframework.http.HttpStatus
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
+import javax.transaction.Transactional
 
 @Service
 class ArticleService(
@@ -42,5 +43,47 @@ class ArticleService(
         return ArticleWriteResponseDto(
             articleRepository.save(article),
             user.email)
+    }
+
+    @Transactional
+    fun edit(
+        articleId: Long,
+        currentURI: String,
+        requestDto: ArticleWriteRequestDto
+        ): ArticleWriteResponseDto {
+        val user = (userRepository.findByEmail(requestDto.email)
+            ?: throw CustomException(
+                status = HttpStatus.BAD_REQUEST,
+                message = "게시글 수정 실패: 해당 email에 해당하는 유저가 없습니다.",
+                requestURI = currentURI
+            ))
+
+        if (!passwordEncoder.matches(requestDto.password, user.password)) {
+            throw CustomException(
+                status = HttpStatus.BAD_REQUEST,
+                message = "게시글 수정 실패: 비밀번호가 일치하지 않습니다.",
+                requestURI = currentURI
+            )
+        }
+
+        val article = articleRepository.findById(articleId)
+            ?: throw CustomException(
+                status = HttpStatus.BAD_REQUEST,
+                message = "게시글 수정 실패: 해당 Id를 가진 게시글이 없습니다.",
+                requestURI = currentURI
+            )
+
+        if (!article.user?.equals(user)!!)
+            throw throw CustomException(
+                status = HttpStatus.BAD_REQUEST,
+                message = "게시글 수정 실패: 해당 게시글은 해당 유저의 소유가 아닙니다.",
+                requestURI = currentURI
+            )
+
+        article.title = requestDto.title
+        article.content = requestDto.content
+
+        return ArticleWriteResponseDto(
+            article, article.user.email)
     }
 }
