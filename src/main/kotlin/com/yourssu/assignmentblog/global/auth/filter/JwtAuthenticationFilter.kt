12f369c89c976b818629d467207664ed4cd72d1a
shon5544 <sbs8239@gmail.com>
2023-09-14 00:33:30 +0900

    add: JwtAuthenticationFilter 추가

---
+++ src/main/kotlin/com/yourssu/assignmentblog/global/auth/filter/JwtAuthenticationFilter.kt
@@ -0,0 +1,16 @@
+package com.yourssu.assignmentblog.global.auth.filter
+
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+class JwtAuthenticationFilter: OncePerRequestFilter() {
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        TODO("Not yet implemented")
+    }
+}
