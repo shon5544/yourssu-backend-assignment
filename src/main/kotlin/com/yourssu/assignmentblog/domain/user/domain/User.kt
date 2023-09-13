835120ebefcba4b7614e102a01fb6e6115193252
shon5544 <sbs8239@gmail.com>
2023-09-14 06:45:16 +0900

    feat: 생성자를 이용한 dto -> entity 변환

--- src/main/kotlin/com/yourssu/assignmentblog/domain/user/domain/User.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/user/domain/User.kt
@@ -3,6 +3,7 @@ package com.yourssu.assignmentblog.domain.user.domain
 import com.yourssu.assignmentblog.domain.article.domain.Article
 import com.yourssu.assignmentblog.domain.comment.domain.Comment
 import com.yourssu.assignmentblog.domain.common.BaseCreateAndUpdateTimeEntity
+import com.yourssu.assignmentblog.domain.user.dto.request.SignupRequestDto
 import com.yourssu.assignmentblog.global.common.enums.Role
 import javax.persistence.*
 
@@ -27,6 +28,14 @@ class User(
     @OneToMany(mappedBy = "user", cascade = [CascadeType.REMOVE])
     val articleList: List<Article> = ArrayList()
 ) : BaseCreateAndUpdateTimeEntity() {
+
+    constructor(signupRequestDto: SignupRequestDto) : this() {
+        this.email = signupRequestDto.email
+        this.password = signupRequestDto.password
+        this.username = signupRequestDto.username
+        this.role = Role.ROLE_USER
+    }
+
     fun updateRefreshToken(refreshToken: String) {
         this.refreshToken = refreshToken
     }
