918dce751271e0777a1a5ee5cba1e9c96257f59c
shon5544 <sbs8239@gmail.com>
2023-09-13 19:19:45 +0900

    feat: 필드 추가

--- src/main/kotlin/com/yourssu/assignmentblog/security/jwt/JwtTokenManager.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/security/jwt/JwtTokenManager.kt
@@ -1,4 +1,31 @@
 package com.yourssu.assignmentblog.security.jwt
 
-class JwtTokenManager {
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+class JwtTokenManager(
+    @Value("\${jwt.secretKey}")
+    private val secretKey: String,
+
+    @Value("\${jwt.access.expiration}")
+    private val accessTokenExpiration: Long,
+
+    @Value("\${jwt.refresh.expiration}")
+    private val refreshTokenExpiration: Long,
+
+    @Value("\${jwt.access.header}")
+    private val accessTokenHeader: String,
+
+    @Value("\${jwt.refresh.header}")
+    private val refreshTokenHeader: String,
+) {
+    companion object {
+        val ACCESS_TOKEN_SUBJECT = "AccessToken"
+        val REFRESH_TOKEN_SUBJECT = "RefreshToken"
+        val EMAIL = "email"
+        val BEARER = "bearer ";
+    }
+
 }
