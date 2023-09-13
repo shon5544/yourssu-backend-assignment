2934dde9fa9406e2a65faafaae130e31f25e0942
shon5544 <sbs8239@gmail.com>
2023-09-13 20:16:48 +0900

    refactor: 필드 정리 및 todo 추가

--- src/main/kotlin/com/yourssu/assignmentblog/security/jwt/JwtTokenManager.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/security/jwt/JwtTokenManager.kt
@@ -9,22 +9,32 @@ class JwtTokenManager(
     private val secretKey: String,
 
     @Value("\${jwt.access.expiration}")
-    private val accessTokenExpiration: Long,
+    private val accessTokenExpiration: Int,
 
     @Value("\${jwt.refresh.expiration}")
-    private val refreshTokenExpiration: Long,
+    private val refreshTokenExpiration: Int,
 
     @Value("\${jwt.access.header}")
     private val accessTokenHeader: String,
 
     @Value("\${jwt.refresh.header}")
-    private val refreshTokenHeader: String,
+    private val refreshTokenHeader: String
 ) {
     companion object {
         const val ACCESS_TOKEN_SUBJECT = "AccessToken"
         const val REFRESH_TOKEN_SUBJECT = "RefreshToken"
         const val EMAIL = "email"
-        const val BEARER = "bearer ";
+        const val BEARER = "bearer "
+        const val AUTHORITES_KEY = "auth"
     }
 
+    /*
+    만들어야 할 메소드
+    - 액세스 토큰 생성
+    - 리프레시 토큰 생성
+    - 액세스 토큰 추출
+    - 리프레시 토큰 추출
+    - 액세스 토큰 검증
+    - 리프레시 토큰 검증
+    */
 }
