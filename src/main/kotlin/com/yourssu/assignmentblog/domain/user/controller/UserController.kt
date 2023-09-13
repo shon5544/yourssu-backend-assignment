0974b46cc5db5499ec6b66c83b6b597102d2c4e0
shon5544 <sbs8239@gmail.com>
2023-09-14 06:47:33 +0900

    feat: UserController 추가

---
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/user/controller/UserController.kt
@@ -0,0 +1,26 @@
+package com.yourssu.assignmentblog.domain.user.controller
+
+import com.yourssu.assignmentblog.domain.user.dto.request.SignupRequestDto
+import com.yourssu.assignmentblog.domain.user.dto.response.SignupResponseDto
+import com.yourssu.assignmentblog.domain.user.service.UserService
+import com.yourssu.assignmentblog.global.common.uri.RequestURI
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
+
+@RestController
+@RequestMapping(RequestURI.USER)
+class UserController(
+    private val userService: UserService
+) {
+
+    @PostMapping("/signup")
+    fun signup(
+        @RequestBody @Valid signupRequestDto: SignupRequestDto): SignupResponseDto {
+        val signupUser = userService.signup(signupRequestDto)
+
+        return SignupResponseDto(signupUser)
+    }
+}
