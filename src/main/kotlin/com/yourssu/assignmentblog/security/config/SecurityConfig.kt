22e478f68d497f587c89fb1a22580eebb9faa874
shon5544 <sbs8239@gmail.com>
2023-09-13 18:30:07 +0900

    feat: SecurityConfig 기본 구성 요소 추가

--- src/main/kotlin/com/yourssu/assignmentblog/security/config/SecurityConfig.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/security/config/SecurityConfig.kt
@@ -1,4 +1,47 @@
 package com.yourssu.assignmentblog.security.config
 
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
 class SecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .formLogin()
+            .disable()
+
+            .httpBasic()
+            .disable()
+
+            .csrf()
+            .disable()
+
+            .headers()
+            .frameOptions()
+            .disable()
+            .and()
+
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+
+            .authorizeHttpRequests()
+            .anyRequest().permitAll()
+
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
 }
