93cee0629448c9bc79ebf164244be1e35f65827d
shon5544 <sbs8239@gmail.com>
2023-09-14 08:31:48 +0900

    feat: ArticleRepository findById 메서드 추가

--- src/main/kotlin/com/yourssu/assignmentblog/domain/article/repository/ArticleRepository.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/article/repository/ArticleRepository.kt
@@ -4,4 +4,5 @@ import com.yourssu.assignmentblog.domain.article.domain.Article
 
 interface ArticleRepository {
     fun save(article: Article): Article
+    fun findById(id: Long): Article?
 }
--- src/main/kotlin/com/yourssu/assignmentblog/domain/article/repository/impl/ArticleRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/article/repository/impl/ArticleRepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.yourssu.assignmentblog.domain.article.repository.impl
 import com.yourssu.assignmentblog.domain.article.domain.Article
 import com.yourssu.assignmentblog.domain.article.repository.ArticleRepository
 import com.yourssu.assignmentblog.domain.article.repository.jpa.ArticleJpaRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
@@ -11,5 +12,9 @@ class ArticleRepositoryImpl(
 ): ArticleRepository {
     override fun save(article: Article): Article {
         return articleJpaRepository.save(article)
+    }
+
+    override fun findById(id: Long): Article? {
+        return articleJpaRepository.findByIdOrNull(id)
     }
 }
