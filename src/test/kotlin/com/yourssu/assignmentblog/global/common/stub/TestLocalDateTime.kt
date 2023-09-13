cc89b5c1b33f18651b553c4763b7bf2b0ca1495e
shon5544 <sbs8239@gmail.com>
2023-09-14 00:32:51 +0900

    test: Stub 객체 추가

---
+++ src/test/kotlin/com/yourssu/assignmentblog/global/common/stub/TestLocalDateTime.kt
@@ -0,0 +1,22 @@
+package com.yourssu.assignmentblog.global.common.stub
+
+import com.yourssu.assignmentblog.global.common.localDateTImeHolder.LocalDateTimeHolder
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+
+class TestLocalDateTime: LocalDateTimeHolder {
+    override fun plusHour(hour: Long): Instant {
+        return LocalDateTime.of(
+            2023,
+            9,
+            13,
+            21,
+            5,
+            0,
+            0)
+            .atZone(ZoneId.of("Asia/Seoul"))
+            .toInstant()
+    }
+
+}
