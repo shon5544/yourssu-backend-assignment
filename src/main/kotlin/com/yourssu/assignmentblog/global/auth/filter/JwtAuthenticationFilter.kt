069d72d441fb4824cf9e5892eb50fc0216733cef
shon5544 <sbs8239@gmail.com>
2023-09-14 00:45:51 +0900

    feat: NO_CHECK_URIS 추가 - NO_CHECK_URIS는 요청 URI로 들어온 것중에 인증 과정 필요없이 넘기고 싶은 URI를 체크하기 위해 만든 것입니다.

--- src/main/kotlin/com/yourssu/assignmentblog/global/auth/filter/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/global/auth/filter/JwtAuthenticationFilter.kt
@@ -1,16 +1,25 @@
 package com.yourssu.assignmentblog.global.auth.filter
 
+import com.yourssu.assignmentblog.global.common.uri.RequestURI
 import org.springframework.web.filter.OncePerRequestFilter
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 class JwtAuthenticationFilter: OncePerRequestFilter() {
+
+    companion object {
+        private val NO_CHECK_URIS: List<String> = arrayListOf(
+            RequestURI.SIGNUP,
+            RequestURI.LOGIN
+        )
+    }
+
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        TODO("Not yet implemented")
+
     }
 }
