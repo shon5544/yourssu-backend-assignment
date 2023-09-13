e34db236d9dd32ca9a81a6c6a4fdc6dc6a0eff60
shon5544 <sbs8239@gmail.com>
2023-09-14 03:19:36 +0900

    feat: UserDetails, UserDetailsService 구현체 추가 - CustomUserDetails: UserDetails의 구현체입니다. controller에서 인증객체에 접근할 때 쉽게 유저정보를 받아올 수 있도록 getUser 메서드를 추가했습니다. 생성할 때 User 정보를 생성자에서 받습니다. - CustomUserDetailsService: UserDetailsService의 구현체입니다. 인증 객체를 생성하도록 loadUserByUsername 메서드를 override 했습니다.

---
+++ src/main/kotlin/com/yourssu/assignmentblog/global/auth/service/CustomUserDetails.kt
@@ -0,0 +1,46 @@
+package com.yourssu.assignmentblog.global.auth.service
+
+import com.yourssu.assignmentblog.domain.user.domain.User
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class CustomUserDetails(
+    val user: User
+): UserDetails {
+    override fun getAuthorities(): MutableCollection<out GrantedAuthority> {
+        val collection = ArrayList<GrantedAuthority>()
+
+        collection.add(GrantedAuthority { user.role.toString() })
+
+        return collection
+    }
+
+    fun getUser(): User {
+        return user
+    }
+
+    override fun getPassword(): String {
+        return user.password
+    }
+
+    override fun getUsername(): String {
+        return user.username
+    }
+
+    // 필요하면 수정
+    override fun isAccountNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isAccountNonLocked(): Boolean {
+        return true
+    }
+
+    override fun isCredentialsNonExpired(): Boolean {
+        return true
+    }
+
+    override fun isEnabled(): Boolean {
+        return true
+    }
+}
