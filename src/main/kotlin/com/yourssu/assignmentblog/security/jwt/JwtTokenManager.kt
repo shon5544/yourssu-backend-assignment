49745ce6110a21737b7e6a4be1b6ba87f5612187
shon5544 <sbs8239@gmail.com>
2023-09-13 19:29:25 +0900

    refactor: const 키워드 추가

--- src/main/kotlin/com/yourssu/assignmentblog/security/jwt/JwtTokenManager.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/security/jwt/JwtTokenManager.kt
@@ -1,6 +1,5 @@
 package com.yourssu.assignmentblog.security.jwt
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
@@ -22,10 +21,10 @@ class JwtTokenManager(
     private val refreshTokenHeader: String,
 ) {
     companion object {
-        val ACCESS_TOKEN_SUBJECT = "AccessToken"
-        val REFRESH_TOKEN_SUBJECT = "RefreshToken"
-        val EMAIL = "email"
-        val BEARER = "bearer ";
+        const val ACCESS_TOKEN_SUBJECT = "AccessToken"
+        const val REFRESH_TOKEN_SUBJECT = "RefreshToken"
+        const val EMAIL = "email"
+        const val BEARER = "bearer ";
     }
 
 }
