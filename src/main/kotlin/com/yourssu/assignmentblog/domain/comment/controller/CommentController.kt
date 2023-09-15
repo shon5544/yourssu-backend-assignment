f9383435d0fa3ede12a85563b09f5bafa5569183
shon5544 <sbs8239@gmail.com>
2023-09-16 04:08:27 +0900

    feat: CommentController edit 추가

--- src/main/kotlin/com/yourssu/assignmentblog/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/comment/controller/CommentController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
 
@@ -26,5 +27,19 @@ class CommentController(
             articleId = articleId,
             currentURI = RequestURI.COMMENT + "/write",
             requestDto = commentWriteRequestDto)
+    }
+
+    @PostMapping("/edit")
+    fun edit(
+        @RequestBody @Valid commentWriteRequestDto: CommentWriteRequestDto,
+        @RequestParam(name = "article") articleId: Long,
+        @RequestParam(name = "comment") commentId: Long
+    ): CommentWriteResponseDto {
+        return commentService.edit(
+            articleId = articleId,
+            commentId = commentId,
+            currentURI = RequestURI.COMMENT + "/edit?article=${articleId}&comment=${commentId}",
+            requestDto = commentWriteRequestDto
+        )
     }
 }
