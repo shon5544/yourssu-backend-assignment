9e4b29d9ad8cb48ef0ec6a02d24dcbe8ff79dd22
shon5544 <sbs8239@gmail.com>
2023-09-16 04:07:04 +0900

    feat: findById 추가 & etc. - findByEmail 삭제 - Comment에는 email 필드가 없는데 findByEmail을 만들었다.

--- src/main/kotlin/com/yourssu/assignmentblog/domain/comment/repository/CommentRepository.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/comment/repository/CommentRepository.kt
@@ -3,6 +3,7 @@ package com.yourssu.assignmentblog.domain.comment.repository
 import com.yourssu.assignmentblog.domain.comment.domain.Comment
 
 interface CommentRepository {
-    fun findByEmail(email: String): Comment?
+    fun findById(id: Long): Comment?
+
     fun save(comment: Comment): Comment
 }
--- src/main/kotlin/com/yourssu/assignmentblog/domain/comment/repository/impl/CommentRepositoryImpl.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/comment/repository/impl/CommentRepositoryImpl.kt
@@ -3,14 +3,15 @@ package com.yourssu.assignmentblog.domain.comment.repository.impl
 import com.yourssu.assignmentblog.domain.comment.domain.Comment
 import com.yourssu.assignmentblog.domain.comment.repository.CommentRepository
 import com.yourssu.assignmentblog.domain.comment.repository.jpa.CommentJpaRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
 
 @Repository
 class CommentRepositoryImpl(
     private val commentJpaRepository: CommentJpaRepository
 ): CommentRepository {
-    override fun findByEmail(email: String): Comment? {
-        return commentJpaRepository.findByEmail(email)
+    override fun findById(id: Long): Comment? {
+        return commentJpaRepository.findByIdOrNull(id)
     }
 
     override fun save(comment: Comment): Comment {
