22b9f3c7e3bd500b43829ecd11b7ac6a6564ae32
shon5544 <sbs8239@gmail.com>
2023-09-16 04:39:40 +0900

    feat: UserController withdraw 메서드 추가

--- src/main/kotlin/com/yourssu/assignmentblog/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/user/controller/UserController.kt
@@ -1,9 +1,12 @@
 package com.yourssu.assignmentblog.domain.user.controller
 
 import com.yourssu.assignmentblog.domain.user.dto.request.SignupRequestDto
+import com.yourssu.assignmentblog.domain.user.dto.request.WithdrawRequestDto
 import com.yourssu.assignmentblog.domain.user.dto.response.SignupResponseDto
 import com.yourssu.assignmentblog.domain.user.service.UserService
 import com.yourssu.assignmentblog.global.common.uri.RequestURI
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -20,5 +23,18 @@ class UserController(
     fun signup(
         @RequestBody @Valid signupRequestDto: SignupRequestDto): SignupResponseDto {
         return userService.signup(signupRequestDto, RequestURI.USER + "/signup")
+    }
+
+    @DeleteMapping("/withdraw")
+    fun withdraw(
+        @RequestBody @Valid withDrawRequestDto: WithdrawRequestDto,
+    ): ResponseEntity<Void> {
+
+        userService.withdraw(
+            requestDto = withDrawRequestDto,
+            currentURI = RequestURI.USER + "/withdraw"
+        )
+
+        return ResponseEntity.ok().build()
     }
 }
