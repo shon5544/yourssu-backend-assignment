fd1eedf712823d992395255ebe61f07a0daef69b
shon5544 <sbs8239@gmail.com>
2023-09-17 06:07:31 +0900

    fix: 토큰이 필요 없도록 수정

--- src/main/kotlin/com/yourssu/assignmentblog/global/auth/filter/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/global/auth/filter/JwtAuthenticationFilter.kt
@@ -47,68 +47,71 @@ class JwtAuthenticationFilter(
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        if (NO_CHECK_URIS.contains(request.requestURI)) {
-            filterChain.doFilter(request, response)
-            return
-        }
+//        if (NO_CHECK_URIS.contains(request.requestURI)) {
+//            filterChain.doFilter(request, response)
+//            return
+//        }
 
-        val accessToken = jwtTokenManager.extractToken(accessTokenHeader, request)
-        val refreshToken = jwtTokenManager.extractToken(refreshTokenHeader, request)
-
-        if (refreshToken != null) {
-            if (!jwtTokenManager.isTokenValid(refreshToken)) {
-                setBadRequestResponse(response, "인증 실패: 유효한 refresh 토큰이 아닙니다. " +
-                        "로그인을 통해 토큰을 재발급 받으세요.")
-
-                return
-            }
-
-            if(!isRefreshTokenInDB(refreshToken)) {
-                setBadRequestResponse(response, "인증 실패: 해당 refresh 토큰에 대한 기록이 없습니다. " +
-                        "로그인을 통해 토큰을 재발급 받으세요.")
-
-                return
-            }
-
-            val reIssuedTokens: ReIssuedTokens = jwtTokenManager.reIssueTokens(refreshToken)
-
-            sendAccessTokenAndRefreshToken(
-                response,
-                reIssuedTokens.accessToken,
-                reIssuedTokens.refreshToken)
-            return
-        }
-
-        if (accessToken != null) {
-            if (!jwtTokenManager.isTokenValid(accessToken)) {
-                setBadRequestResponse(response, "인증 실패: 유효한 access token이 아닙니다. " +
-                        "로그인을 통해 토큰을 재발급 받으세요.")
-
-                return
-            }
-
-            val userEmail = jwtTokenManager.extractEmailFromToken(accessToken)
-            if (userEmail == null) {
-                setBadRequestResponse(response, "인증 실패: access token에 email claim이 없습니다.")
-                return
-            }
-
-            val user = userRepository.findByEmail(userEmail)
-
-            if (user == null) {
-                setBadRequestResponse(response, "인증 실패: access token에 기재된 email을 가진 User가 없습니다.")
-
-                return
-            }
-
-            setAuthentication(user)
-
-            filterChain.doFilter(request, response)
-            return
-        }
-
-        setBadRequestResponse(response, "인증 실패: access token, refresh token 모두 누락되었습니다.")
+        filterChain.doFilter(request, response)
         return
+
+//        val accessToken = jwtTokenManager.extractToken(accessTokenHeader, request)
+//        val refreshToken = jwtTokenManager.extractToken(refreshTokenHeader, request)
+//
+//        if (refreshToken != null) {
+//            if (!jwtTokenManager.isTokenValid(refreshToken)) {
+//                setBadRequestResponse(response, "인증 실패: 유효한 refresh 토큰이 아닙니다. " +
+//                        "로그인을 통해 토큰을 재발급 받으세요.")
+//
+//                return
+//            }
+//
+//            if(!isRefreshTokenInDB(refreshToken)) {
+//                setBadRequestResponse(response, "인증 실패: 해당 refresh 토큰에 대한 기록이 없습니다. " +
+//                        "로그인을 통해 토큰을 재발급 받으세요.")
+//
+//                return
+//            }
+//
+//            val reIssuedTokens: ReIssuedTokens = jwtTokenManager.reIssueTokens(refreshToken)
+//
+//            sendAccessTokenAndRefreshToken(
+//                response,
+//                reIssuedTokens.accessToken,
+//                reIssuedTokens.refreshToken)
+//            return
+//        }
+//
+//        if (accessToken != null) {
+//            if (!jwtTokenManager.isTokenValid(accessToken)) {
+//                setBadRequestResponse(response, "인증 실패: 유효한 access token이 아닙니다. " +
+//                        "로그인을 통해 토큰을 재발급 받으세요.")
+//
+//                return
+//            }
+//
+//            val userEmail = jwtTokenManager.extractEmailFromToken(accessToken)
+//            if (userEmail == null) {
+//                setBadRequestResponse(response, "인증 실패: access token에 email claim이 없습니다.")
+//                return
+//            }
+//
+//            val user = userRepository.findByEmail(userEmail)
+//
+//            if (user == null) {
+//                setBadRequestResponse(response, "인증 실패: access token에 기재된 email을 가진 User가 없습니다.")
+//
+//                return
+//            }
+//
+//            setAuthentication(user)
+//
+//            filterChain.doFilter(request, response)
+//            return
+//        }
+//
+//        setBadRequestResponse(response, "인증 실패: access token, refresh token 모두 누락되었습니다.")
+//        return
     }
 
     @Throws(IOException::class)
