04cde31ffe81646551b663556eeb16e9a5a6995f
shon5544 <sbs8239@gmail.com>
2023-09-17 02:49:06 +0900

    feat: 실패한 요소에 대한 정보 추가

---
+++ src/main/kotlin/com/yourssu/assignmentblog/global/common/enums/FailedMethod.kt
@@ -0,0 +1,9 @@
+package com.yourssu.assignmentblog.global.common.enums
+
+class FailedMethod {
+    companion object {
+        const val WRITE = "작성"
+        const val EDIT = "수정"
+        const val DELETE = "삭제"
+    }
+}
---
+++ src/main/kotlin/com/yourssu/assignmentblog/global/common/enums/FailedTargetType.kt
@@ -0,0 +1,9 @@
+package com.yourssu.assignmentblog.global.common.enums
+
+class FailedTargetType {
+    companion object {
+        const val ARTICLE = "게시글"
+        const val COMMENT = "댓글"
+        const val USER = "유저"
+    }
+}
