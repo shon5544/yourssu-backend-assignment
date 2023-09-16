35726f5a6efef99843251d2fcb067afe4f56b221
shon5544 <sbs8239@gmail.com>
2023-09-17 04:44:33 +0900

    refactor: 가입, 탈퇴 시 유저 존재 확인 로직 책임 위임

--- src/main/kotlin/com/yourssu/assignmentblog/domain/user/service/UserService.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/user/service/UserService.kt
@@ -5,8 +5,9 @@ import com.yourssu.assignmentblog.domain.user.dto.request.SignupRequestDto
 import com.yourssu.assignmentblog.domain.user.dto.request.WithdrawRequestDto
 import com.yourssu.assignmentblog.domain.user.dto.response.SignupResponseDto
 import com.yourssu.assignmentblog.domain.user.repository.UserRepository
-import com.yourssu.assignmentblog.global.error.exception.CustomException
-import org.springframework.http.HttpStatus
+import com.yourssu.assignmentblog.global.common.domain.ExistenceChecker
+import com.yourssu.assignmentblog.global.common.enums.FailedMethod
+import com.yourssu.assignmentblog.global.common.enums.FailedTargetType
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 import javax.transaction.Transactional
@@ -14,19 +15,21 @@ import javax.transaction.Transactional
 @Service
 class UserService(
     private val userRepository: UserRepository,
-    private val passwordEncoder: BCryptPasswordEncoder
+    private val passwordEncoder: BCryptPasswordEncoder,
+    private val existenceChecker: ExistenceChecker
 ) {
 
     @Transactional
     fun signup(requestDto: SignupRequestDto, currentURI: String): SignupResponseDto {
         requestDto.password = passwordEncoder.encode(requestDto.password)
 
-        if (userRepository.findByEmail(requestDto.email) != null)
-            throw CustomException(
-                status = HttpStatus.BAD_REQUEST,
-                message = "회원가입 실패: 해당 email로 이미 가입이 되어있습니다.",
-                requestURI = currentURI
-            )
+        val failedTargetText = "${FailedTargetType.USER} ${FailedMethod.SIGNUP}"
+
+        existenceChecker.checkUserEmail(
+            email = requestDto.email,
+            failedTargetText = failedTargetText,
+            currentURI = currentURI
+        )
 
         val user = User(requestDto)
 
@@ -38,20 +41,14 @@ class UserService(
         requestDto: WithdrawRequestDto,
         currentURI: String
     ) {
-        val user = (userRepository.findByEmail(requestDto.email)
-            ?: throw CustomException(
-                status = HttpStatus.BAD_REQUEST,
-                message = "유저 탈퇴 실패: 해당 email에 해당하는 유저가 없습니다.",
-                requestURI = currentURI
-            ))
+        val failedTargetText = "${FailedTargetType.USER} ${FailedMethod.WITHDRAW}"
 
-        if (!passwordEncoder.matches(requestDto.password, user.password)) {
-            throw CustomException(
-                status = HttpStatus.BAD_REQUEST,
-                message = "유저 탈퇴 실패: 비밀번호가 일치하지 않습니다.",
-                requestURI = currentURI
-            )
-        }
+        val user = existenceChecker.checkUserAccount(
+            currentURI = currentURI,
+            email = requestDto.email,
+            password = requestDto.password,
+            failedTargetText = failedTargetText
+        )
 
         userRepository.delete(user)
     }
