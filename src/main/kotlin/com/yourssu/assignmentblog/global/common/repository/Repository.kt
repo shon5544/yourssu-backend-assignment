9232db35d3997f543d91f3405526f344a953cbba
shon5544 <sbs8239@gmail.com>
2023-09-17 05:10:10 +0900

    remove: Repository interface 삭제 - 얘는 동적으로 받아내게 하기 힘들다. - ExistenceChecker를 Bean으로 쓰는 상황에서 DI로 repository를 받기엔 어려울 듯하다. - 차라리 repository를 함수의 인자로 받아서 넘길까도 생각해봤지만 매개변수 수가 너무 과해져 오히려 코드 가독성이 떨어질 것 같다는 생각이 들었다. - 이건 차차 생각해보자

--- src/main/kotlin/com/yourssu/assignmentblog/global/common/repository/Repository.kt
+++
@@ -1,7 +0,0 @@
-package com.yourssu.assignmentblog.global.common.repository
-
-import com.yourssu.assignmentblog.global.common.entity.EntityWithOwnership
-
-interface Repository {
-    fun findById(id: Long): EntityWithOwnership?
-}
