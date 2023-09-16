438d5f3d801097310d7e1496b4a358f0bcd6b883
shon5544 <sbs8239@gmail.com>
2023-09-17 04:41:32 +0900

    feat: SIGNUP, WITHDRAW 행위 추가

--- src/main/kotlin/com/yourssu/assignmentblog/global/common/enums/FailedMethod.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/global/common/enums/FailedMethod.kt
@@ -5,5 +5,7 @@ class FailedMethod {
         const val WRITE = "작성"
         const val EDIT = "수정"
         const val DELETE = "삭제"
+        const val SIGNUP = "가입"
+        const val WITHDRAW = "탈퇴"
     }
 }
