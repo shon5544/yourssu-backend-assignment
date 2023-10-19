f991af4e4538a476628d8140402fa73ac5cc76ae
shon5544 <sbs8239@gmail.com>
2023-10-20 01:02:25 +0900

    feat: HandlerArgumentResolver 구현체 추가

---
+++ src/main/kotlin/com/yourssu/assignmentblog/global/util/resolver/UserAuthArgumentResolver.kt
@@ -0,0 +1,39 @@
+package com.yourssu.assignmentblog.global.util.resolver
+
+import com.yourssu.assignmentblog.global.auth.jwt.AuthInfo
+import com.yourssu.assignmentblog.global.auth.jwt.JwtTokenManager
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.MethodParameter
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+import javax.servlet.http.HttpServletRequest
+
+@Component
+class UserAuthArgumentResolver(
+    @Value("\${jwt.access.header}")
+    private val accessTokenHeader: String,
+
+    private val tokenManager: JwtTokenManager
+): HandlerMethodArgumentResolver {
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.parameterType == AuthInfo::class.java
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?
+    ): Any {
+        val httpServletRequest: HttpServletRequest = webRequest.nativeRequest as HttpServletRequest
+
+        val token: String = tokenManager.extractToken(accessTokenHeader, httpServletRequest)
+
+        val userEmail: String = tokenManager.extractEmailFromToken(token)
+
+        return AuthInfo(userEmail)
+    }
+}
