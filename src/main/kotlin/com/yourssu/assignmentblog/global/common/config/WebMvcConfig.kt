a5c130b27c540c755ec9892dfb1e8601fc2b3fd7
shon5544 <sbs8239@gmail.com>
2023-10-26 04:32:10 +0900

    fix: Bean 중복 문제로 자식 클래스로 지정

--- src/main/kotlin/com/yourssu/assignmentblog/global/common/config/WebMvcConfig.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/global/common/config/WebMvcConfig.kt
@@ -1,12 +1,13 @@
 package com.yourssu.assignmentblog.global.common.config
 
+import com.yourssu.assignmentblog.global.util.resolver.UserAuthArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebMvcConfig(
-    private val userArgumentResolver: HandlerMethodArgumentResolver
+    private val userArgumentResolver: UserAuthArgumentResolver
 ): WebMvcConfigurer {
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
