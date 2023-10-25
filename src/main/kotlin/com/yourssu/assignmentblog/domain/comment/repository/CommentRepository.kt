28a87a23e8a21be17a45493661774a47d55d3939
shon5544 <sbs8239@gmail.com>
2023-10-26 05:10:31 +0900

    fix: Repository 추상화 파일들 하나로 합침

--- src/main/kotlin/com/yourssu/assignmentblog/domain/comment/repository/CommentRepository.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/comment/repository/CommentRepository.kt
@@ -1,10 +1,34 @@
 package com.yourssu.assignmentblog.domain.comment.repository
 
 import com.yourssu.assignmentblog.domain.comment.domain.Comment
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Repository
+
+
+interface CommentJpaRepository: JpaRepository<Comment, Long> {
+}
 
 interface CommentRepository {
     fun findById(id: Long): Comment?
 
     fun save(comment: Comment): Comment
     fun delete(comment: Comment)
+}
+
+@Repository
+class CommentRepositoryImpl(
+    private val commentJpaRepository: CommentJpaRepository
+): CommentRepository {
+    override fun findById(id: Long): Comment? {
+        return commentJpaRepository.findByIdOrNull(id)
+    }
+
+    override fun save(comment: Comment): Comment {
+        return commentJpaRepository.save(comment)
+    }
+
+    override fun delete(comment: Comment) {
+        commentJpaRepository.delete(comment)
+    }
 }
--- src/main/kotlin/com/yourssu/assignmentblog/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/user/repository/UserRepository.kt
@@ -1,10 +1,40 @@
 package com.yourssu.assignmentblog.domain.user.repository
 
 import com.yourssu.assignmentblog.domain.user.domain.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+interface UserJpaRepository: JpaRepository<User, Long?> {
+    fun findByEmail(email: String): User?
+    fun findByRefreshToken(refreshToken: String): User?
+}
+
 
 interface UserRepository {
     fun findByEmail(email: String): User?
     fun findByRefreshToken(refreshToken: String): User?
     fun save(user: User): User
     fun delete(user: User)
+}
+@Repository
+class UserRepositoryImpl(
+    private val userJpaRepository: UserJpaRepository
+): UserRepository {
+
+    override fun findByEmail(email: String): User? {
+        return userJpaRepository.findByEmail(email)
+    }
+
+    override fun findByRefreshToken(refreshToken: String): User? {
+        return userJpaRepository.findByRefreshToken(refreshToken)
+    }
+
+    override fun save(user: User): User {
+        return userJpaRepository.save(user)
+    }
+
+    override fun delete(user: User) {
+        userJpaRepository.delete(user)
+    }
+
 }
