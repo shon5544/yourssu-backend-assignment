733459b337515e588d1458af82dd219647ce9041
shon5544 <sbs8239@gmail.com>
2023-10-26 04:08:36 +0900

    refactor: JwtAuthenticationFilter의 각 기능에 대해서 책임 위임 및 분기 처리 리팩토링

--- src/main/kotlin/com/yourssu/assignmentblog/global/auth/filter/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/global/auth/filter/JwtAuthenticationFilter.kt
@@ -1,14 +1,14 @@
 package com.yourssu.assignmentblog.global.auth.filter
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.yourssu.assignmentblog.domain.user.domain.User
 import com.yourssu.assignmentblog.domain.user.repository.UserRepository
-import com.yourssu.assignmentblog.global.auth.dto.response.AuthenticationFailDto
-import com.yourssu.assignmentblog.global.auth.dto.response.TokenReIssueResponseDto
-import com.yourssu.assignmentblog.global.auth.jwt.JwtTokenManager
 import com.yourssu.assignmentblog.global.auth.jwt.ReIssuedTokens
+import com.yourssu.assignmentblog.global.auth.jwt.token.TokenChecker
+import com.yourssu.assignmentblog.global.auth.jwt.token.TokenExtractor
+import com.yourssu.assignmentblog.global.auth.jwt.token.TokenProvider
 import com.yourssu.assignmentblog.global.auth.service.CustomUserDetails
 import com.yourssu.assignmentblog.global.common.uri.RequestURI
+import com.yourssu.assignmentblog.global.util.sender.ResponseSender
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper
@@ -16,7 +16,6 @@ import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.filter.OncePerRequestFilter
-import java.io.IOException
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
@@ -26,9 +25,7 @@ class JwtAuthenticationFilter(
 
     private val refreshTokenHeader: String,
 
-    private val jwtTokenManager: JwtTokenManager,
-
-    private val objectMapper: ObjectMapper,
+    private val tokenProvider: TokenProvider,
 
     private val userRepository: UserRepository,
 
@@ -52,94 +49,39 @@ class JwtAuthenticationFilter(
             return
         }
 
-        val accessToken = jwtTokenManager.extractToken(accessTokenHeader, request)
-        val refreshToken = jwtTokenManager.extractToken(refreshTokenHeader, request)
+        val accessToken = TokenExtractor.extractToken(accessTokenHeader, request, response)
+        val refreshToken = TokenExtractor.extractToken(refreshTokenHeader, request, response)
 
         if (refreshToken != null) {
-            if (!jwtTokenManager.isTokenValid(refreshToken)) {
-                setBadRequestResponse(response, "인증 실패: 유효한 refresh 토큰이 아닙니다. " +
-                        "로그인을 통해 토큰을 재발급 받으세요.")
 
+            if(!TokenChecker.isRefreshTokenInDB(refreshToken)) {
+                ResponseSender.setBadRequestResponse(response, "인증 실패: 해당 refresh 토큰에 대한 기록이 없습니다. " +
+                        "로그인을 통해 토큰을 재발급 받으세요.")
                 return
             }
 
-            if(!isRefreshTokenInDB(refreshToken)) {
-                setBadRequestResponse(response, "인증 실패: 해당 refresh 토큰에 대한 기록이 없습니다. " +
-                        "로그인을 통해 토큰을 재발급 받으세요.")
-
-                return
-            }
-
-            val reIssuedTokens: ReIssuedTokens = jwtTokenManager.reIssueTokens(refreshToken)
-
-            sendAccessTokenAndRefreshToken(
-                response,
-                reIssuedTokens.accessToken,
-                reIssuedTokens.refreshToken)
+            val reIssuedTokens: ReIssuedTokens = tokenProvider.reIssueTokens(refreshToken)
+            ResponseSender.sendAccessTokenAndRefreshToken(response, reIssuedTokens)
             return
         }
 
         if (accessToken != null) {
-            if (!jwtTokenManager.isTokenValid(accessToken)) {
-                setBadRequestResponse(response, "인증 실패: 유효한 access token이 아닙니다. " +
-                        "로그인을 통해 토큰을 재발급 받으세요.")
 
-                return
-            }
-
-            val userEmail = jwtTokenManager.extractEmailFromToken(accessToken)
-            if (userEmail == null) {
-                setBadRequestResponse(response, "인증 실패: access token에 email claim이 없습니다.")
-                return
-            }
-
+            val userEmail = TokenExtractor.extractEmailFromToken(accessToken, response)
             val user = userRepository.findByEmail(userEmail)
 
             if (user == null) {
-                setBadRequestResponse(response, "인증 실패: access token에 기재된 email을 가진 User가 없습니다.")
-
+                ResponseSender.setBadRequestResponse(response, "인증 실패: access token에 기재된 email을 가진 User가 없습니다.")
                 return
             }
 
             setAuthentication(user)
-
             filterChain.doFilter(request, response)
             return
         }
 
-        setBadRequestResponse(response, "인증 실패: access token, refresh token 모두 누락되었습니다.")
+        ResponseSender.setBadRequestResponse(response, "인증 실패: access token, refresh token 모두 누락되었습니다.")
         return
-    }
-
-    @Throws(IOException::class)
-    private fun setBadRequestResponse(response: HttpServletResponse, message: String) {
-        val responseDto = AuthenticationFailDto(false, message)
-        val result: String = objectMapper.writeValueAsString(responseDto)
-
-        response.status = HttpServletResponse.SC_BAD_REQUEST
-        response.characterEncoding = "UTF-8"
-        response.contentType = "application/json"
-        response.writer.write(result)
-    }
-
-    private fun sendAccessTokenAndRefreshToken(
-        response: HttpServletResponse,
-        accessToken: String,
-        refreshToken: String) {
-
-        val responseDto = TokenReIssueResponseDto(true, "토큰 재발급 성공.", accessToken, refreshToken)
-        val result = objectMapper.writeValueAsString(responseDto)
-
-        response.status = HttpServletResponse.SC_BAD_REQUEST
-        response.characterEncoding = "UTF-8"
-        response.contentType = "application/json"
-        response.writer.write(result)
-    }
-
-    private fun isRefreshTokenInDB(refreshToken: String): Boolean {
-        val user = userRepository.findByRefreshToken(refreshToken)
-
-        return user != null
     }
 
     private fun setAuthentication(user: User) {
