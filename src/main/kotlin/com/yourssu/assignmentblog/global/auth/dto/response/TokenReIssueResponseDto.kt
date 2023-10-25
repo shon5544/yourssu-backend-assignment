19d6fb739f9cc3aac405a99669d0e4a3a8293d5e
shon5544 <sbs8239@gmail.com>
2023-10-26 04:03:51 +0900

    remove: TokenReIssueResponseDto.kt 삭제

--- src/main/kotlin/com/yourssu/assignmentblog/global/auth/dto/response/TokenReIssueResponseDto.kt
+++
@@ -1,8 +0,0 @@
-package com.yourssu.assignmentblog.global.auth.dto.response
-
-data class TokenReIssueResponseDto(
-    val success: Boolean = false,
-    val message: String = "",
-    val accessToken: String = "",
-    val refreshToken: String = ""
-)
