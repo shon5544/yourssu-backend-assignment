0405f8233eaeb725c57cc5fac09a6989022c7bca
shon5544 <sbs8239@gmail.com>
2023-10-27 04:38:12 +0900

    fix: Auth 필드 Swagger에 노출되지 않도록 수정

--- src/main/kotlin/com/yourssu/assignmentblog/global/util/annotation/Auth.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/global/util/annotation/Auth.kt
@@ -1,5 +1,8 @@
 package com.yourssu.assignmentblog.global.util.annotation
 
+import io.swagger.v3.oas.annotations.media.Schema
+
 @Target(AnnotationTarget.VALUE_PARAMETER)
 @Retention(AnnotationRetention.RUNTIME)
+@Schema(accessMode = Schema.AccessMode.READ_ONLY)
 annotation class Auth
