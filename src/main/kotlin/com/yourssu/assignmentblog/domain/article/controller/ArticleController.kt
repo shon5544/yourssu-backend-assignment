7e650cac6d1edf8e7a5bcc2bc772c0ba939a8b70
shon5544 <sbs8239@gmail.com>
2023-10-27 04:42:02 +0900

    feat: API Operation 추가

--- src/main/kotlin/com/yourssu/assignmentblog/domain/article/controller/ArticleController.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/article/controller/ArticleController.kt
@@ -6,6 +6,8 @@ import com.yourssu.assignmentblog.domain.article.service.ArticleService
 import com.yourssu.assignmentblog.global.auth.jwt.AuthInfo
 import com.yourssu.assignmentblog.global.common.uri.RequestURI
 import com.yourssu.assignmentblog.global.util.annotation.Auth
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -23,6 +25,7 @@ class ArticleController(
 ) {
 
     @PostMapping("/write")
+    @Operation(summary = "게시글 작성", description = "title, content를 입력받아 게시글을 작성합니다")
     fun write(
         @RequestBody @Valid requestDto: ArticleRequestDto,
         @Auth authInfo: AuthInfo
@@ -35,6 +38,8 @@ class ArticleController(
     }
 
     @PutMapping("/edit/{articleId}")
+    @Operation(summary = "게시글 수정", description = "원하는 게시글을 수정합니다.")
+    @Parameter(name = "article id", description = "수정하려는 게시글의 id 값입니다.")
     fun edit(
         @RequestBody @Valid articleRequestDto: ArticleRequestDto,
         @PathVariable articleId: Long,
@@ -49,6 +54,8 @@ class ArticleController(
     }
 
     @DeleteMapping("/delete/{articleId}")
+    @Operation(summary = "게시글 삭제", description = "게시글을 삭제합니다.")
+    @Parameter(name = "article id", description = "삭제하려는 게시글의 id 값입니다.")
     fun delete(
         //@RequestBody @Valid deleteRequestDto: DeleteRequestDto,
         @PathVariable articleId: Long,
