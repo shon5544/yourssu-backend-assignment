f8f9d071ba1a4b7366d498430663d94927eda263
shon5544 <sbs8239@gmail.com>
2023-10-30 19:17:38 +0900

    feat: AOP를 Trailing lambda 표현 - ExistenceChecker, OwnershipChecker를 사용하는 로직들을 AOP로 분리했습니다.

---
+++ src/main/kotlin/com/yourssu/assignmentblog/global/common/aop/ExistenceCheckAdviceHolder.kt
@@ -0,0 +1,83 @@
+package com.yourssu.assignmentblog.global.common.aop
+
+import com.yourssu.assignmentblog.global.common.domain.ExistenceChecker
+import org.springframework.stereotype.Component
+
+@Component
+class ExistenceCheckAdviceHolder(
+    _existenceCheckAdvice: ExistenceCheckAdvice
+) {
+    init {
+        existenceCheckAdvice = _existenceCheckAdvice
+    }
+
+    companion object {
+        private lateinit var existenceCheckAdvice: ExistenceCheckAdvice
+
+        fun <T> checkUserAccount(
+            currentURI: String,
+            email: String,
+            failedTargetText: String,
+            function: () -> T
+        ): T {
+            return existenceCheckAdvice.checkUserAccount(
+                currentURI,
+                email,
+                failedTargetText,
+                function
+            )
+        }
+
+        fun <T> checkArticleExistence(
+            articleId: Long,
+            currentURI: String,
+            failedTargetText: String,
+            function: () -> T
+        ): T {
+            return existenceCheckAdvice.checkArticleExistence(
+                articleId,
+                currentURI,
+                failedTargetText,
+                function
+            )
+        }
+    }
+
+    @Component
+    class ExistenceCheckAdvice(
+        private val existenceChecker: ExistenceChecker
+    ) {
+
+        fun <T> checkUserAccount(
+            currentURI: String,
+            email: String,
+            failedTargetText: String,
+            function: () -> T
+        ): T {
+
+            existenceChecker.checkUserAccount(
+                currentURI,
+                email,
+                failedTargetText
+            )
+
+            return function.invoke()
+        }
+
+        fun <T> checkArticleExistence(
+            articleId: Long,
+            currentURI: String,
+            failedTargetText: String,
+            function: () -> T
+        ): T {
+
+            existenceChecker.checkArticle(
+                articleId,
+                failedTargetText,
+                currentURI
+            )
+
+            return function.invoke()
+        }
+    }
+}
