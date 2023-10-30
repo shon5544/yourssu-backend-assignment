dc153eae602dc7446f4e515da87c60471ed0cb9c
shon5544 <sbs8239@gmail.com>
2023-10-30 20:14:49 +0900

    fix: UserService에 AOP 적용

--- src/main/kotlin/com/yourssu/assignmentblog/domain/user/service/UserService.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/user/service/UserService.kt
@@ -4,7 +4,7 @@ import com.yourssu.assignmentblog.domain.user.domain.User
 import com.yourssu.assignmentblog.domain.user.dto.request.SignupRequestDto
 import com.yourssu.assignmentblog.domain.user.dto.response.SignupResponseDto
 import com.yourssu.assignmentblog.domain.user.repository.UserRepository
-import com.yourssu.assignmentblog.global.common.domain.ExistenceChecker
+import com.yourssu.assignmentblog.global.common.aop.ExistenceCheckAdviceHolder
 import com.yourssu.assignmentblog.global.common.enums.FailedMethod
 import com.yourssu.assignmentblog.global.common.enums.FailedTargetType
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
@@ -15,33 +15,32 @@ import javax.transaction.Transactional
 class UserService(
     private val userRepository: UserRepository,
     private val passwordEncoder: BCryptPasswordEncoder,
-    private val existenceChecker: ExistenceChecker
 ) {
 
     @Transactional
-    fun signup(requestDto: SignupRequestDto, currentURI: String): SignupResponseDto {
+    fun signup(
+        requestDto: SignupRequestDto
+    ): SignupResponseDto = ExistenceCheckAdviceHolder.checkUserEmailNotExist(
+        email = requestDto.email, currentURI = requestDto.currentURI
+    ) {
         requestDto.password = passwordEncoder.encode(requestDto.password)
-
-        existenceChecker.checkUserEmailNotExist(requestDto.email, currentURI)
 
         val user = User(requestDto)
 
-        return SignupResponseDto(userRepository.save(user))
+        return@checkUserEmailNotExist SignupResponseDto(userRepository.save(user))
     }
 
     @Transactional
     fun withdraw(
         email: String,
-        currentURI: String
+        currentURI: String,
+        failedTargetText: String = "${FailedTargetType.USER} ${FailedMethod.WITHDRAW}"
+    ) = ExistenceCheckAdviceHolder.checkUserAccount(
+        currentURI = currentURI,
+        email = email,
+        failedTargetText = failedTargetText
     ) {
-        val failedTargetText = "${FailedTargetType.USER} ${FailedMethod.WITHDRAW}"
-
-        val user = existenceChecker.checkUserAccount(
-            currentURI = currentURI,
-            email = email,
-            failedTargetText = failedTargetText
-        )
-
+        val user: User = userRepository.findByEmail(email)!!
         userRepository.delete(user)
     }
 }
