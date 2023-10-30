11c7ef9e2a7afd219f08606f41fc1dd8504b503c
shon5544 <sbs8239@gmail.com>
2023-10-30 19:28:37 +0900

    fix: CommentRequestDto가 BaseRequestDto 상속하도록 수정

--- src/main/kotlin/com/yourssu/assignmentblog/domain/comment/dto/request/CommentRequestDto.kt
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/comment/dto/request/CommentRequestDto.kt
@@ -1,9 +1,10 @@
 package com.yourssu.assignmentblog.domain.comment.dto.request
 
+import com.yourssu.assignmentblog.global.common.dto.BaseRequestDto
 import javax.validation.constraints.NotBlank
 
 data class CommentRequestDto(
 
     @field: NotBlank(message = "content는 누락되면 안 됩니다.")
     val content: String = ""
-)
+): BaseRequestDto()
