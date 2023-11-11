f964b7e4a6bef7799614718eceb835384a13729b
Beomsu Son <65557784+shon5544@users.noreply.github.com>
2023-11-12 00:19:30 +0900

    refactor: OwnershipCheckAspect에 대한 의존성 주입 문제 해결

---
+++ src/main/kotlin/com/yourssu/assignmentblog/global/common/aop/OwnershipCheckAdvice.kt
@@ -0,0 +1,29 @@
+package com.yourssu.assignmentblog.global.common.aop
+
+import com.yourssu.assignmentblog.domain.user.domain.User
+import com.yourssu.assignmentblog.global.common.entity.EntityWithOwnership
+import com.yourssu.assignmentblog.global.error.exception.CustomException
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+
+@Component
+class OwnershipCheckAdvice {
+
+    companion object {
+        fun <T> checkOwnership(
+            target: EntityWithOwnership,
+            currentURI: String,
+            user: User,
+            failedTargetText: String,
+            function: () -> T
+        ): T {
+            if (target.user != user)
+                throw CustomException(
+                    status = HttpStatus.BAD_REQUEST,
+                    message = "$failedTargetText 실패: 해당 ${failedTargetText.split(" ")[0]}은 해당 유저의 소유가 아닙니다.",
+                    requestURI = currentURI)
+
+            return function.invoke()
+        }
+    }
+}
