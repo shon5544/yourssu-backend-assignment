c731e4b8b78384906d0181b8ffcee341b660522e
shon5544 <sbs8239@gmail.com>
2023-11-19 18:23:31 +0900

    feat: Admin용 User 확인 API DTO 추가

---
+++ src/main/kotlin/com/yourssu/assignmentblog/domain/user/dto/request/GetUsersRequestDto.kt
@@ -0,0 +1,10 @@
+package com.yourssu.assignmentblog.domain.user.dto.request
+
+import java.time.LocalDate
+
+data class GetUsersRequestDto(
+    val username: String?,
+    val email: String?,
+    val createdAtStart: LocalDate?,
+    val createdAtEnd: LocalDate?
+)
